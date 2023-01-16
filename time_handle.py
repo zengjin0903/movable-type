9067a0d08c60308b19e1db64dc7de1643f40f34a
zengjin0903 <54163659+zengjin0903@users.noreply.github.com>
2023-01-16 19:10:26 +0800

    Update time_handle.py

--- time_handle.py
+++ time_handle.py
@@ -25,6 +25,14 @@ def time_handle(data, time_col):
     该函数负责将数据集中的时间字符串处理成时间格式，并且判断是否中国的节假日
     data:数据集,time_col数据集中时间戳的列表名字
     返回新的数据框,包含是否节假日，月份，周几，一年第几周，年份
+
+    需要数据集中有时间列（time_col）和数值列 （value_col）的格式如下：
+    ...  | time_col  |  value_col1  | value_col2 |
+     0   | 20210101  |    34523     |    43212   |
+     1   | 20210102  |    34343     |    43512   |
+     2   | 20210103  |    34343     |    43512   |
+    ……
+    
     """
     data[time_col] = data[time_col].map(lambda x: transefer_to_date(x))
     data["is_holiday"] = data[time_col].map(lambda x: is_holiday(x))
